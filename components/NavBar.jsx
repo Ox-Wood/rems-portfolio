3e73f9c301ea0bbe9027a45a1f8a625bc08643f7
Rem <rllerado@gmail.com>
2025-02-23 01:35:14 +0800

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -39,7 +39,7 @@ const NavBar = () => {
     <main>
         <div onClick={navbarphoneanimationopen} className='w-[50px] h-[50px] xl:opacity-0 xl:z-0 cursor-pointer top-2 right-2 bg-[#09122C] z-10 rounded-xl absolute flex justify-center items-center text-white'><RxHamburgerMenu /></div>
         <header id='navbarphone-menu' className='w-full h-24 flex justify-center xl:items-center bg-[#09122C] max-sm:w-screen max-sm:fixed max-sm:h-screen max-sm:-right-[520px] z-10' >
-          <nav id='mainbox' className='px-[5px] py-[5px] flex xl:items-center max-sm:flex-col xl:border-b border-white'>
+          <nav id='mainbox' className='px-[5px] py-[5px] flex items-center justify-center max-sm:flex-col xl:border-b border-white'>
               <div onClick={navbarphoneanimationclose} className='text-white cursor-pointer flex items-center justify-center max-sm:w-screen max-sm:h-[8vh] opacity-0 max-sm:opacity-100 xl:absolute' ><RxCross1 /></div>
               <div className='w-[40vw] py-[10px] text-white flex justify-evenly items-center max-sm:w-screen max-sm:flex-col max-sm:h-[30vh]'>
                 <a id='navtext1' className='px-9 py-3 uppercase text-sm' href="#">home</a>
