065f061fe6d2e2a6ec9639e80b837c5ed9982329
Rem <rllerado@gmail.com>
2025-02-24 12:39:08 +0800

    Update page.jsx

--- app/RecentWorks/page.jsx
+++ app/RecentWorks/page.jsx
@@ -9,8 +9,8 @@ const page = () => {
     <main className='bg-[#F8F5E9]'>
       <NavBar />
       <div className=' w-full h-[86.5vh] max-sm:h-[100vh] flex justify-center'>
-        <section className='w-[90vw] h-[80vh] mt-2 max-sm:h-[95vh] border overflow-hidden' >
-          <div className='font-Anton mb-5' >
+        <section className='w-[90vw] h-[80vh] mt-2 max-sm:h-[95vh] overflow-hidden' >
+          <div className='font-Anton mb-5 max-sm:mt-9' >
               <h1 className='text-6xl max-sm:text-4xl text-[#ff4655]'>My Recent Works</h1>
           </div>
           <div className='w-[22vw] h-[8vh] flex justify-between max-sm:w-[75vw]'>
@@ -24,15 +24,20 @@ const page = () => {
                 <h1 className='max-sm:text-sm' >VIDEOS</h1>
               </div>
           </div>
-          <div className='w-full h-[80vh] overflow-hidden'>
+          <div className='w-full'>
             <div className='relative w-full h-[80vh] overflow-x-hidden'>
-                <div className='w-[90vw]  absolute grid max-sm:grid-cols-2 gap-2'>
-                    <div className='w-[42vw] h-[25vh] rounded-xl justify-self-center bg-white'></div>
-                    <div className='w-[42vw] h-[35vh] rounded-xl justify-self-center bg-black'></div>
-                    <div className='w-[42vw] h-[25vh] rounded-xl justify-self-center bg-white'></div>
-                    <div className='w-[42vw] h-[35vh] rounded-xl justify-self-center bg-black'></div>
-                    <div className='w-[42vw] h-[35vh] rounded-xl justify-self-center bg-black'></div>
-                    <div className='w-[42vw] h-[35vh] rounded-xl justify-self-center bg-black'></div>
+                <div className='w-[90vw] absolute max-sm:columns-2 columns-5 gap-4 space-y-4'>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[25vh] h-[45vh] rounded-xl bg-white break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[25vh] h-[45vh] rounded-xl bg-white break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[35vh] h-[40vh] rounded-xl bg-black break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[25vh] h-[45vh] rounded-xl bg-white break-inside-avoid'></div>
+                    <div className='max-sm:w-[42vw] w-[17.5vw] max-sm:h-[25vh] h-[45vh] rounded-xl bg-white break-inside-avoid'></div>
                 </div>
             </div>
           </div>
