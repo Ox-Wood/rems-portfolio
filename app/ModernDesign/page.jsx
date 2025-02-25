b701194374ed784c224c8a192a90ee2135b97949
Rem <rllerado@gmail.com>
2025-02-25 16:40:43 +0800

    Update page.jsx

--- app/ModernDesign/page.jsx
+++ app/ModernDesign/page.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <main>
-        <div className="max-sm:invisible max-sm:absolute -z-50" ><ModernNavBar /></div>
-        <div className="max-sm:opacity-100 absolute max-sm:top-5 max-sm:right-5 z-50 -top-96"><MobileModernNavBar /></div>
+        <ModernNavBar />
     </main>
   )
 }
