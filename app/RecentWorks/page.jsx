82df10a6002905430987a6e158c2c99de3e80a68
Rem <rllerado@gmail.com>
2025-02-26 17:59:47 +0800

    Update page.jsx

--- app/RecentWorks/page.jsx
+++ app/RecentWorks/page.jsx
@@ -32,7 +32,7 @@ const page = () => {
                 <div className='w-[90vw] max-sm:columns-2 columns-5 gap-4 space-y-2'>
                 
                 {recentprojects.map(({id, source, size, mobile}) => (
-                  <div key={id} className={`max-sm:w-[42vw] w-[17.5vw] ${size} border max-sm:h-[25vh] rounded-xl break-inside-avoid overflow-hidden `}>
+                  <div key={id} className={`max-sm:w-[42vw] w-[17.5vw] ${size} border max-sm:${mobile} rounded-xl break-inside-avoid overflow-hidden `}>
                       {source.slice(-3) === "png" || source.slice(-3) ==="jpg" ?(
                         <img 
                         src={source}
