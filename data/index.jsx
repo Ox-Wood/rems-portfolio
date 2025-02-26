4c15d38d54f6b56536e8a12c1a0bed1b85d2c96c
Rem <rllerado@gmail.com>
2025-02-26 17:06:36 +0800

    Update index.jsx

--- data/index.jsx
+++ data/index.jsx
@@ -3,34 +3,34 @@ export const recentprojects = [{
     id: 1,
     source: "/latest-works/2.png",
     size: "55vh",
-    mobile: "25vh"
+    mobile: "20vh"
 
 },{
 
     id: 2,
     source: "/latest-works/3.png",
     size: "40vh",
-    mobile: "35vh"
+    mobile: "30vh"
 
 },{
 
     id: 3,
     source: "/latest-works/4.jpg",
     size: "55vh",
-    mobile: "25vh"
+    mobile: "20vh"
 
 },{
 
     id: 4,
     source: "/latest-works/12.png",
     size: "40vh",
-    mobile: "35vh"
+    mobile: "30vh"
 
 },{
 
     id: 5,
     source: "/video/video bg.mp4",
     size: "55vh",
-    mobile: "25vh"
+    mobile: "20vh"
 
 }]
